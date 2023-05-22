6d5c0318cdd3113de71060407bc97c26967cd2f9
saidikrom29 <saidikrom2901>
2023-05-22 14:59:23 +0500

    2-ish

--- isOdd.dart
+++ isOdd.dart
@@ -1,4 +1,7 @@
 // Create a variable "number" and assign it a integer value.
 
 // Print 1 if the number is odd, otherwise print 0.
-void main() {}
+void main() {
+  int a = 7;
+  print((a + 1) % 2);
+}
