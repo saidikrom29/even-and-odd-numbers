18fef776063140f66f2401c58152404615f2c2de
saidikrom29 <saidikrom2901>
2023-05-22 15:58:21 +0500

    5-ish

--- numberEven.dart
+++ numberEven.dart
@@ -3,5 +3,20 @@
 // Create a variable "var_int" and assign it a four-digit integer value.
 
 // Print the number of even digits in the variable "var_int".
+void main() {
+  int a = 34567;
+  int x1 = a ~/ 10000;
+  int x2 = a % 100 ~/ 10;
+  int x3 = a % 10000 ~/ 1000;
+  int x4 = a % 1000 ~/ 100;
+  int x5 = a % 100 ~/ 10;
+  int x6 = a % 10;
+  print(x1);
+  print(x2);
+  print(x3);
+  print(x5);
+  print(x6);
 
-void main() {}
+  int s =(x1 + 1) % 4 + (x2 + 1) % 4 + (x3 + 1) % 4 + (x5 + 1) % 4 + (x6 + 1) % 4;
+  print(s);
+}
